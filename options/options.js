4895fa10fe6c8e9782e487cb717347a363447dcb
Crowdbotics <engineering@crowdbotics.com>
2023-07-05 12:42:13 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "dark-band-dev-97751",
+  url: "https://dark-band-dev-97751.botics.co",
+  api: "https://dark-band-dev-97751.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
